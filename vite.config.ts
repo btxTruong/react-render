b3cc215c4d229a18c0d3df1b951f2fc9e113bc2a
Truong Bui <btxtruong@gmail.com>
2024-03-28 14:44:14 +0700

    chore: update logic to find source in github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import packageJson from './package.json'
+
+/*
+ * We need repo name to correct endpoint of github page
+ */
+const REPO = packageJson.name
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,8 +20,8 @@ export default defineConfig({
         index: 'index.html',
       },
       output: {
-        chunkFileNames: `[name].[hash].js`,
-        entryFileNames: `[name].js`,
+        chunkFileNames: `${REPO}/[name].[hash].js`,
+        entryFileNames: `${REPO}/[name].js`,
         assetFileNames: `[name].[ext]`,
         sourcemapExcludeSources: true,
       },
